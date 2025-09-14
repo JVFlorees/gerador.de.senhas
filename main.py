392bf8e6c880bea902054155e1de5d7e1c899ce3
JVFlorees <victorjoao7579@gmail.com>
2025-09-13 22:25:35 -0300

    Este é um projeto simples de um gerador de senhas.

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+#Escolha quantos caracteres tera sua senha
+tamanho_senha = int(input("Escolha o tamanho da senha: "))
+
+def gerar_senha(comprimento):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
+    return senha
+
+# Exemplo de uso: gerar senha com comprimento 12
+senha_gerada = gerar_senha(tamanho_senha)
+print("Senha gerada:", senha_gerada)
