7df833d936e02bc2e215a30ccfc41b933548e328
JVFlorees <victorjoao7579@gmail.com>
2025-09-14 22:06:28 -0300

    Implementação do Flask e interface web: integração do gerador de senhas com página HTML, formulários para gerar e limpar senha, e link com CSS para estilização.

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+from flask import Flask, render_template, request
 import random
 import string
 
-#Escolha quantos caracteres tera sua senha.
-tamanho_senha = int(input("Escolha o tamanho da senha: "))
+app = Flask(__name__)
 
+# Função para gerar a senha
 def gerar_senha(comprimento):
     caracteres = string.ascii_letters + string.digits + string.punctuation
     senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
     return senha
 
-# Exemplo de uso: gerar senha com comprimento 12
-senha_gerada = gerar_senha(tamanho_senha) 
-print("Senha gerada:", senha_gerada)
+# Rota principal
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    senha_gerada = None
+    if request.method == 'POST':
+        tamanho = int(request.form['tamanho'])
+        acao = (request.form['acao'])
+        if acao =="gerar":
+            senha_gerada = gerar_senha(tamanho)
+        elif acao == "limpar":
+            senha_gerada = None
+    return render_template('home.html', senha=senha_gerada)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
